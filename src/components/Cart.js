c4361cab909ca44f8480cd19f5ac1a869b906eae
NOKAYA Jean-Michel <nokaya.jeanmichel@gmail.com>
2021-12-23 13:49:28 +0100

    done

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,15 +1,29 @@
 import { useState, useEffect } from 'react'
 import '../styles/Cart.css'
 
+
 function Cart({ cart, updateCart }) {
 	const [isOpen, setIsOpen] = useState(true)
+	const [isEdit, setIsEdit] = useState(false)
+	let tempCart = cart
 	const total = cart.reduce((iResult, { price, quantity }) => {
 		return iResult + (price * quantity)
 	}, 0)
 	useEffect(() => {
 		document.title = `LMJ: ${total}€ d'achats`
 	}, [total])
-	
+	function handleChangeQuantity(sName, iQuantity){
+		let oPlant = cart.find(({name})=> name === sName)
+		if(!oPlant) throw new Error()
+		const filteredCart = cart.filter(({name})=> name !== sName)
+		oPlant.quantity = iQuantity
+		tempCart = [...filteredCart, oPlant]
+		// if(!iQuantity || iQuantity <= 0){
+		// 	updateCart(filteredCart)
+		// }else{
+		// 	updateCart([...filteredCart,oPlant])
+		// }		
+	}
 	return isOpen ? (
 		<div className='lmj-cart'>
 			<button
@@ -18,7 +32,23 @@ function Cart({ cart, updateCart }) {
 			>
 				Fermer
 			</button>
-			{cart.length > 0 ? (
+			{cart.length > 0 ? isEdit ? (
+				<div>
+					<h2>Panier</h2>
+					<ul className="lmj-cart-ul">
+						{cart.map(({ name, price, quantity }, iIndex) => (
+							<div key={`${name}-${iIndex}`}>
+								{name}
+								<input type="number" name={name} defaultValue={quantity} onChange={(e)=>handleChangeQuantity(name, e.target.value)} />
+								x {price}€
+							</div>
+						))}
+					</ul>
+					<h3>Total: {total}€</h3>
+					<button onClick={() => { setIsEdit(false); updateCart(cart) }}>Confirmer les modifications</button>
+					<button onClick={() => { updateCart([]) }}>Vider le panier</button>
+				</div>
+			) : (
 				<div>
 					<h2>Panier</h2>
 					<ul className="lmj-cart-ul">
@@ -29,6 +59,7 @@ function Cart({ cart, updateCart }) {
 						))}
 					</ul>
 					<h3>Total: {total}€</h3>
+					<button onClick={() => { setIsEdit(true) }}>Editer le panier</button>
 					<button onClick={() => { updateCart([]) }}>Vider le panier</button>
 				</div>
 			) : (
